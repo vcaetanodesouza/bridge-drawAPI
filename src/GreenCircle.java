2621629df7a9c5da78ee2e16170ac533998acb21
Vinícius Caetano <49098435+vcaetanodesouza@users.noreply.github.com>
2023-05-24 20:25:46 -0300

    Add files via upload

---
+++ src/GreenCircle.java
@@ -0,0 +1,7 @@
+public class GreenCircle implements DrawAPI{
+
+    @Override
+    public void drawCircle(int radius, int x, int y) {
+        System.out.println("Drawning Circle[ color: green, radius: " + radius + ", x: " + x + ", "+ y + "]");
+    }   
+}
